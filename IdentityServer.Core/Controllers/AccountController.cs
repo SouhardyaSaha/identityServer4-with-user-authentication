ef5f764498ee582266b4dae8cd51af6a6b63f66f
souhardyasaha98 <souhardyasaha98@gmail.com>
2022-03-31 12:08:33 +0600

    Authentication with view

--- IdentityServer.Core/Controllers/AccountController.cs
+++ IdentityServer.Core/Controllers/AccountController.cs
@@ -1,4 +1,6 @@
-﻿using IdentityServer.Core.Models;
+﻿using System.Security.Claims;
+using IdentityModel;
+using IdentityServer.Core.Models;
 using IdentityServer4.Services;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
@@ -30,9 +32,53 @@ public class AccountController : Controller
 
         return View(vm);
     }
-}
 
-public class LoginViewModel
-{
-    public string ReturnUrl { get; set; }
+    [HttpGet]
+    public async Task<IActionResult> Logout(string logoutId)
+    {
+        if (User?.Identity?.IsAuthenticated == true)
+        {
+            // delete local authentication cookie
+            await _signInManager.SignOutAsync();
+        }
+
+        var logout = await _interaction.GetLogoutContextAsync(logoutId);
+
+        return Redirect(logout.PostLogoutRedirectUri);
+    }
+
+    public IActionResult Register()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Register(RegisterViewModel model)
+    {
+        var user = new AppUser
+        {
+            UserName = model.Email,
+            Email = model.Email,
+            EmailConfirmed = true,
+        };
+
+        var result = await _userManager.CreateAsync(user, model.Password);
+
+        if (!result.Succeeded)
+        {
+            throw new Exception(result.Errors.First().Description);
+        }
+
+        result = await _userManager.AddClaimsAsync(user, new Claim[]
+        {
+            new Claim(JwtClaimTypes.Email, model.Email)
+        });
+
+        if (!result.Succeeded)
+        {
+            throw new Exception(result.Errors.First().Description);
+        }
+
+        return View("RegistrationSuccess");
+    }
 }
